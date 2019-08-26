c8278bbbfb2acd7293675694d2aa72ab8c6c70ca
pouyapanahandeh <panahandehpooya@gmail.com>
2019-08-26 10:16:44 +0200

    project 9, middleware

--- 8/src/App.vue
+++ 8/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox 
+            :question="questions[index]"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -14,6 +22,28 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  // as you can see, we write data in different way, but still data include the information of the 
+  // application and returns opbject data.
+  data(){
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  // Mounting hooks are often the most-used hooks, for better or worse. They allow you to access your component immediately before and after the first render. They do not, however, run during server-side rendering.
+  mounted: function(){
+    // Use if: You need to access or modify the DOM of your component immediately before or after the initial render.
+    // Do not use if: You need to fetch some data for your component on initialization. Use created (or created + activated for keep-alive components) for this instead, especially if you need that data during server-side rendering.
+    fetch('https://opentdb.com/api.php?amount=10&category=19&type=multiple',{
+      method:'get'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((jsonData) =>{
+      this.questions = jsonData.results
+    })
   }
 }
 </script>
--- 8/src/components/QuestionBox.vue
+++ 8/src/components/QuestionBox.vue
@@ -1,10 +1,29 @@
     <template>
-        <h1>Question BOX</h1>  
+        <div class="question-box-container">
+            <b-jumbotron>
+                <template slot="lead">
+                   {{ currentQuestion.question }} 
+                </template>
+
+                <hr class="my-4" />
+                
+                <p>
+                    list of answer
+                </p>
+
+                <b-button variant="primary" href="#">Submit</b-button>
+                <b-button variant="success" href="#">Next</b-button>
+            </b-jumbotron>
+        </div>  
     </template>
     
     <script>
     export default {
-    
+        // any data that we want to show from parent to child we need to pass it in props
+        // props is object
+        props: {
+            question: Object,
+        }
     }
     </script>
     
